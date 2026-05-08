226dd5022e324b3c91852df729b5a2ff14a30fb8
mirzatechno <baignaveed799@gmail.com>
2026-05-08 18:43:02 +0530

    Update SunglassesApplication.java

--- backend/src/main/java/com/example/sunglasses/SunglassesApplication.java
+++ backend/src/main/java/com/example/sunglasses/SunglassesApplication.java
@@ -2,9 +2,17 @@ package com.example.sunglasses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SunglassesApplication {
+public class SunglassesApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        return application.sources(SunglassesApplication.class);
+    }
 
     public static void main(String[] args) {
 
